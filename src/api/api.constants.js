3ad9b2bd79e882cb89a6f826697af03ce5564eb7
Dima <zahrebelny@gmail.com>
2021-02-21 19:56:33 +0200

    updated requests

--- src/api/api.constants.js
+++ src/api/api.constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://jsonplaceholder.typicode.com';
+export const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 export const ENDPOINT_MAP = {
   posts: '/posts',
--- src/api/api.js
+++ src/api/api.js
@@ -6,18 +6,36 @@ export const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
 
-export const getAllPosts = () => axiosInstance.get(ENDPOINT_MAP.posts);
-export const getPost = (id) => axiosInstance.get(`${ENDPOINT_MAP.posts}/${id}`);
+export const getAllPosts = () =>
+  axiosInstance.get(ENDPOINT_MAP.posts, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
+export const getPost = (id) =>
+  axiosInstance.get(`${ENDPOINT_MAP.posts}/${id}`, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
 export const creatPost = (data) =>
   axiosInstance.post(ENDPOINT_MAP.posts, data, {
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
+      'Access-Control-Allow-Origin': '*',
     },
   });
 export const updatePost = (data) =>
   axiosInstance.put(`${ENDPOINT_MAP.posts}/${data.id}`, data, {
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
+      'Access-Control-Allow-Origin': '*',
     },
   });
-export const deletePost = (id) => axiosInstance.delete(`${ENDPOINT_MAP.posts}/${id}`);
+export const deletePost = (id) =>
+  axiosInstance.delete(`${ENDPOINT_MAP.posts}/${id}`, {
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
